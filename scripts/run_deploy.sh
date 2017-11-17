98bbc2e3d0c3d5cd2e180760842bc5cb704d8968
debi123 <debarati.sinharoy@emc.com>
2017-11-17 14:22:53 +0530

    Update run_deploy.sh

--- scripts/run_deploy.sh
+++ scripts/run_deploy.sh
@@ -3,20 +3,20 @@
 ##cp spark-env.sh /home/build-spark/rootfs/usr/spark-2.2.0/conf
 ##chmod 777 spark-env.sh
 
-cp trial.csv /home
-chmod 777 trial.csv
+# cp trial.csv /home
+# chmod 777 trial.csv
 
 ##. /opt/hbase-server > a.out
 
-apt-get -y update
+#apt-get -y update
 
-apt-get install -y openssh-server
+#apt-get install -y openssh-server
 
-/opt/hbase/bin/start-hbase.sh
+#/opt/hbase/bin/start-hbase.sh
 
-/opt/hbase/bin/hbase-daemons.sh start zookeeper
+#/opt/hbase/bin/hbase-daemons.sh start zookeeper
 
-/opt/hbase-server
+#/opt/hbase-server
 
 
 ##/opt/hbase/bin/start-hbase.sh
