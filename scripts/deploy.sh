b63577dbf6cb41a180ac9935584a991ba2a89f0b
debi123 <debarati.sinharoy@emc.com>
2017-11-17 11:47:39 +0530

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,8 @@
 
 ls
 
-cp create.sql /home
-chmod 777 create.sql
+cp run.py /home
+chmod 777 run.py
 
 apk update
 
@@ -16,12 +16,12 @@ apk add sshpass
 
 apk add scp
 
-sshpass -p 'gpadmin' scp -o "StrictHostKeyChecking no" create_gp.sql gpadmin@10.63.33.203:/home/gpadmin/ftp
+sshpass -p 'Welcome@321' scp -o "StrictHostKeyChecking no" run.py hduser1@10.63.33.207:/home/hduser1/ftp
 
 
-sshpass -p 'gpadmin' ssh -o "StrictHostKeyChecking no" gpadmin@10.63.33.203 <<!
-ls /home/gpadmin/ftp/*
-psql -a -f "/home/gpadmin/ftp/create_gp.sql"
+sshpass -p 'gpadmin' ssh -o "StrictHostKeyChecking no" hduser1@10.63.33.207 <<!
+ls /home/hduser1/ftp/*
+# psql -a -f "/home/gpadmin/ftp/create_gp.sql"
 ##psql -c "CREATE schema trial"
 ##psql -c "CREATE TABLE trial.weather (city int, name varchar(10)) DISTRIBUTED BY (city)"
 ##psql -c "insert into trial.weather (city,name) values (1,'aaa')"
