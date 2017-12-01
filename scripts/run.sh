ed568a26b0c1b9fdd4f6a166e804c3b1816d6edf
debi123 <debarati.sinharoy@emc.com>
2017-12-01 10:40:54 +0530

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,20 +6,20 @@ python --version
 ##cp spark-env.sh /home/build-spark/rootfs/usr/spark-2.2.0/conf
 ##chmod 777 spark-env.sh
 
-# cp trial.csv /home
-# chmod 777 trial.csv
+cp trial.csv /home
+chmod 777 trial.csv
 
 ##. /opt/hbase-server > a.out
 
-# apt-get -y update
+apt-get -y update
 
-# apt-get install -y openssh-server
+apt-get install -y openssh-server
 
-# /opt/hbase/bin/start-hbase.sh
+/opt/hbase/bin/start-hbase.sh
 
 # /opt/hbase/bin/hbase-daemons.sh start zookeeper
 
-# /opt/hbase-server
+/opt/hbase-server
 
 
 ##/opt/hbase/bin/start-hbase.sh
@@ -28,7 +28,7 @@ python --version
 
 ##hbase shell
 
-# hbase shell ./sample_commands.txt
+hbase shell ./sample_commands.txt
 
 ##echo "list" | hbase shell
 
